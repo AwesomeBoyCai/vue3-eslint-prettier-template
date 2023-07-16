315dcca25b7fb4c27c16ffbc7d6765abd98fb949
Cai <1357618897@qq.com>
2023-07-16 21:02:55 +0800

    build(options): 📦️ 搭建模板

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vue3-eslint-prettier-template
+
+![image-20230716192120529](https://awesomeboy.oss-cn-chengdu.aliyuncs.com/img/202307161921567.png)
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>hello world</h1>
+  </div>
+</template>
+
+<script lang="ts"></script>
+
+<style lang="" scoped></style>
