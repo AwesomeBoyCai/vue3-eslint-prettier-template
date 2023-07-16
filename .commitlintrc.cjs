e4dd6230db109b8737e677c6d843d64561a7f65a
Cai <1357618897@qq.com>
2023-07-16 21:13:00 +0800

    feat(deps): ✨ 增加scope可选项

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -11,8 +11,6 @@ module.exports = {
       subject: '填写简短精炼的变更描述 :\n',
       body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
       breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
-      footerPrefixsSelect: '选择关联issue前缀（可选）:',
-      customFooterPrefixs: '输入自定义issue前缀 :',
       footer: '列举关联issue (可选) 例如: #31, #I3244 :\n',
       confirmCommit: '是否提交或修改commit ?'
     },
@@ -46,6 +44,32 @@ module.exports = {
       }
     ],
     useEmoji: true,
+    // scope 类型（定义之后，可通过上下键选择）
+    scopes: [
+      ['components', '组件相关'],
+      ['hooks', 'hook 相关'],
+      ['utils', 'utils 相关'],
+      ['element-ui', '对 element-ui 的调整'],
+      ['styles', '样式相关'],
+      ['deps', '项目依赖'],
+      ['auth', '对 auth 修改'],
+      ['other', '其他修改']
+    ].map(([value, description]) => {
+      return {
+        value,
+        name: `${value.padEnd(30)} (${description})`
+      }
+    }),
+
+    // 是否允许自定义填写 scope，在 scope 选择的时候，会有 empty 和 custom 可以选择。
+    allowCustomScopes: true,
+
+    // 跳过要询问的步骤
+    skipQuestions: ['body', 'breaking', 'footer'],
+    subjectLimit: 100, // subject 限制长度
+    // 设置只有 type 选择了 feat 或 fix，才询问 breaking message
+    allowBreakingChanges: ['feat', 'fix'],
+
     issuePrefixs: [
       // 如果使用 gitee 作为开发管理
       { value: 'link', name: 'link:     链接 ISSUES 进行中' },
